6541209c1d22dc3bf591c01093c824ad12af702e
markhorvath <markhorvath7@gmail.com>
2018-02-09 10:49:11 -0500

    lec 129 added posts_new

---
+++ src/components/posts_new.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class PostsNew extends Component {
+  render() {
+    return (
+      <div>
+        Posts New
+      </div>
+    );
+  }
+}
+
+export default PostsNew;
