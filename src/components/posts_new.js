b9a72981f84b5c1387e7bef972a7acfe40f631f9
markhorvath <markhorvath7@gmail.com>
2018-02-09 16:33:23 -0500

    lec133 redux-form added to posts_new component

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import { Field, reduxFrom } from 'redux-form';
 
 class PostsNew extends Component {
   render() {
     return (
-      <div>
-        Posts New
-      </div>
+      <form>
+        <Field
+          name="title"
+          component={}
+        />
+      </form>
     );
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: 'PostsNewForm'
+})(PostsNew);
