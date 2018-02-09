e970902c2a04cd9ccae19b1898a4476370b53167
markhorvath <markhorvath7@gmail.com>
2018-02-09 11:30:45 -0500

    lec131, added Link tag to posts_index from react-router-dom for nav

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchPosts } from '../actions';
 
 
@@ -20,9 +21,16 @@ class PostsIndex extends Component {
   }
 
   render() {
-    console.log(this.props.posts);
     return (
+      // When you click on a Link tag it has a couple of event handers on it that prevent
+      // the browser from doing what it normally does, which is to navigate or issue another
+      // http request to fetch another html document from the server
       <div>
+        <div className="text-xs-right">
+          <Link className="btn btn-primary" to="/posts/new">
+            Add a Post
+          </Link>
+        </div>
         <h3>Posts</h3>
         <ul className="list-group">
           {this.renderPosts()}
